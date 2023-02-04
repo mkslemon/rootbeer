89d79c510bd5d5c5983b591c69a98bbc8c564b58
NAPTlME <napratt2@yahoo.com>
2023-02-04 03:18:07 -0800

    Adding the Drink and Patron classes
    The Drink class holds the state of any mixed drink. It also contains some functions for getting the mixed flavor as well as mixed color of the drink.
    Note: the `GetColor` function has a few blend modes that need to be tested.
    
    The Patron class is a monobehavior in this case to allow for serializing of fields. The Patron class contains a flavor profile, List of Preferred and Hated Toppings.
    
    Note the FlavorProfile is not showing up appropriately in the inspector at this time.

---
+++ Assets/Scripts/Drink.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ggj.rootbeer
+{
+    public class Drink
+    {
+        public List<Syrup> Syrups;
+        public List<Topping> Toppings;
+
+        public Drink()
+        {
+            Syrups = new List<Syrup>();
+            Toppings = new List<Topping>();
+        }
+
+        public FlavorProfile GetFlavor()
+        {
+            // get average of all flavors present
+            // can also add flavors to toppings, but ignoring for now until further clarification, also the toppings would have less influence so that would need to be accounted for
+            List<float[]> flavorArrays = Syrups.Select(sel => sel.FlavorProfile.GetAsArray()).ToList();
+            var AverageFlavor = Enumerable.Range(1, flavorArrays[0].Length).Select(i => flavorArrays.Select(flavor => flavor[i]).Sum() / flavorArrays.Count).ToArray();
+            return new FlavorProfile(AverageFlavor);
+        }
+        public Color GetColor()
+        {
+            // try different blend methods?
+            // some examples altered from here: https://stackoverflow.com/questions/1351442/is-there-an-algorithm-for-color-mixing-that-works-like-mixing-real-colors
+            Color returnColor = Color.black;
+            int blendMode = 0;
+
+            switch(blendMode)
+            {
+                case 1:
+                    // simple subtractive?
+                    returnColor = Syrups.Select(sel => sel.Color).
+                        Aggregate((c1, c2) => new Color(c1.r * c2.r, c1.g * c2.g, c1.b * c2.g));
+                    break;
+                case 2:
+                    // dilute subtractive?
+                    returnColor.r = 1 - Mathf.Sqrt(Syrups.Select(sel => Mathf.Pow(1 - sel.Color.r, 2)).Average());
+                    returnColor.g = 1 - Mathf.Sqrt(Syrups.Select(sel => Mathf.Pow(1 - sel.Color.g, 2)).Average());
+                    returnColor.b = 1 - Mathf.Sqrt(Syrups.Select(sel => Mathf.Pow(1 - sel.Color.b, 2)).Average());
+                    break;
+                default:
+                    // basic average
+                    returnColor.r = Syrups.Select(sel => sel.Color.r).Average();
+                    returnColor.g = Syrups.Select(sel => sel.Color.g).Average();
+                    returnColor.b = Syrups.Select(sel => sel.Color.b).Average();
+                    break;
+            }
+            return returnColor;
+        }
+    }
+}
