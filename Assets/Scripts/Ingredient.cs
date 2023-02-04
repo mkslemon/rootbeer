d96816cb20edd5a7d85ed5a0fcba4f5664d7895f
Dan Birman <danbirman@gmail.com>
2023-02-04 12:17:54 -0800

    Fixing various bugs

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -7,11 +7,22 @@ public class Ingredient : MonoBehaviour
     private FlavorTooltip _flavorTooltip;
 
     private void Awake() {
-        _flavorTooltip = GameObject.Find("FlavorTooltip").GetComponent<FlavorTooltip>();
+        _flavorTooltip = GameObject.Find("IngredientFlavorTooltip").GetComponent<FlavorTooltip>();
+        _flavorTooltip.gameObject.SetActive(false);
     }
 
     private void OnMouseEnter() {
+
+        // set the _flavorTooltip position to the screen position
+        Vector3 screenPosition = Input.mousePosition;
+
+        screenPosition.z = _flavorTooltip.gameObject.transform.position.z;
+        Debug.Log(screenPosition);
+
+        _flavorTooltip.gameObject.transform.position = screenPosition;
         _flavorTooltip.gameObject.SetActive(true);
+
+
     }
 
     private void OnMouseExit() {
