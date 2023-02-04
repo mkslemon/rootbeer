5b92eec031045923916d8f7a4f8454108459a70d
NAPTlME <napratt2@yahoo.com>
2023-02-04 02:25:26 -0800

    Adding Syrups and Toppings Class
    The Syrups Class has the following:
    - A name for display
    - Text for optional flavor text within the UI
    - Color Used for shaders and for mixing colors for the drink shader
    
    The Toppings Class contains:
    - ToppingName An Enum of potential toppings
    - Text for optional flavor text in the ui
    - a function `GetPrettyName` which returns the string formatted without underscores for display

---
+++ Assets/Scripts/Topping.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ggj.rootbeer
+{
+    public enum ToppingName { Orange_Peel, Coconut, Candy_Cane }
+    public class Topping
+    {
+        public ToppingName Name { get; set; }
+        public string Text { get; set; }
+
+        public Topping(ToppingName name, string text = "")
+        {
+            Name = name;
+            Text = text;
+        }
+
+        public string GetPrettyName()
+        {
+            return this.Name.ToString().Replace('_', ' ');
+        }
+    }
+}
