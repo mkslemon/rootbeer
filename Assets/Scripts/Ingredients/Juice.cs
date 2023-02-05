63ed19dabfb878de644b7d43ca120625d61b8047
NAPTlME <napratt2@yahoo.com>
2023-02-04 21:23:27 -0800

    letting juice inherit from syrup

--- Assets/Scripts/Ingredients/Juice.cs
+++ Assets/Scripts/Ingredients/Juice.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace ggj.rootbeer {
-    public class Juice {
-        public string Name { get; set; }
-        public Color Color { get; set; }
-        public string Text { get; set; }
-        public FlavorProfile FlavorProfile { get; }
+    public class Juice:  Syrup
+    {
 
-        public Juice(string name, Color? color = null, string text = "", FlavorProfile flavorProfile = null) {
-            Name = name;
-            Color = color ?? Color.black; // doing this weird thing due to the value for this parameter needing to be a compile time constant, might not be necessary to make it an optional parameter, but this works
-            Text = text;
-            FlavorProfile = flavorProfile ?? new FlavorProfile();
+        public Juice(string name, Color? color = null, string text = "", FlavorProfile flavorProfile = null) : base(name, color, text, flavorProfile) 
+        {
+            // currently no new functionality for Juice
         }
     }
 }
