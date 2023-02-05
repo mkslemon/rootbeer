3a64c3a690fc0e22cd747fd48678afd25c5fc569
mkslemon <matt.lemonpepper@gmail.com>
2023-02-05 01:03:49 -0800

    ui more

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,12 +49,12 @@ namespace ggj.rootbeer
             _ingredients[2]._juice = new Juice("Sparkling OJ", Color.red, "", new FlavorProfile(1f, 0f, 1f, 0f));
 
             _ingredients[3]._syrup = new Syrup("Vanilla", Color.red, "", new FlavorProfile(0f, 1f, 1f, 0f));
-            _ingredients[4]._syrup = new Syrup("Grenadine", Color.red, "", new FlavorProfile(1f, 0f, 1f, 0f));
-            _ingredients[5]._syrup = new Syrup("Coconut Creamer", Color.red, "", new FlavorProfile(0f, 1f, 0f, 1f));
+            _ingredients[4]._syrup = new Syrup("Grenadine", Color.red, "", new FlavorProfile(1f, 1f, 0f, 0f));
+            _ingredients[5]._syrup = new Syrup("Coconut Creamer", Color.red, "", new FlavorProfile(1f, 0f, 0f, 1f));
 
-            _ingredients[6]._topping = new Topping("Mint", "", new FlavorProfile(0f, 1f, 1f, 0f));
-            _ingredients[7]._topping = new Topping("Cherry", "", new FlavorProfile(1f, 0f, 0f, 1f));
-            _ingredients[8]._topping = new Topping("Lime", "", new FlavorProfile(1f, 1f, 0f, 0f));
+            _ingredients[6]._topping = new Topping("Mint", "", new FlavorProfile(0f, .5f, .5f, .5f));
+            _ingredients[7]._topping = new Topping("Cherry", "", new FlavorProfile(.5f, .5f, .5f, 0f));
+            _ingredients[8]._topping = new Topping("Lime", "", new FlavorProfile(.5f, .5f, 0f, .5f));
         }
 
 
